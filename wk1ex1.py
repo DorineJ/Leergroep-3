52b7e17578e02ac3baba4945337f22553650e39f
DorineJ <70524955+DorineJ@users.noreply.github.com>
2020-09-10 11:18:10 +0200

    Create wk1ex1.py
    Deze eerste opdracht is van Dorine

---
+++ wk1ex1.py
@@ -0,0 +1,28 @@
+# Workshop Digitale Technologie 1
+# Bestandsnaam: wk1ex1.py
+# Naam:
+# Probleemomschrijving: het vier-vierenprobleem
+
+from math import *
+
+print("Nul is", 4+4-4-4)
+print("Een is", 4/4)
+print("Twee is", (4+4)/4)
+print("Drie is", (4+4+4)/4)
+print("Vier is", 4)
+print("Vijf is", 4+(4/4))
+print("Zes is", ((4+4+4+4+4+4+4+4+4+4)/4) -4)
+print("Zeven is", (4+4)-(4/4))
+print("Acht is", 4+4)
+print("Negen is", 4+4+(4/4))
+print("Tien is", 4+4 + ((4+4)/4))
+print("Elf is", 4+4+4-(4/4))
+print("Twaalf is", 4+4+4)
+print("Dertien is", 4+4+4 + (4/4))
+print("Veertien is", 4+4+4 + (sqrt(4)))
+print("Vijftien is", 4+4+4+4 - (4/4))
+print("Zestien is", 4+4+4+4)
+print("Zeventien is", 4+4+4+4 + (4/4))
+print("Achtien is", (factorial(4))-4-(sqrt(4)))
+print("Negentien is", (factorial(4))-4-(4/4))
+print("Twintig is", (factorial(4))-4)
