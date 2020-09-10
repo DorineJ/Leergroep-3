ce85624097ce99165f1ce708f6ef97c17691cf10
robinmpotze <70520188+robinmpotze@users.noreply.github.com>
2020-09-10 11:20:03 +0200

    Create Robin - wk1ex1.py

---
+++ Robin - wk1ex1.py
@@ -0,0 +1,44 @@
+from math import *
+
+nul = 4-4
+print("Nul is", nul)
+een = 44/44
+print("Een is", een)
+twee = 4/4 + 4/4
+print("Twee is", twee)
+drie = (4+4+4)/4
+print("Drie is", drie)
+vier = (4*4)/4
+print("Vier is", vier)
+vijf = ((4*4)+4)/4
+print("Vijf is", vijf)
+zes = ((4+4)/4)+4
+print("Zes is", zes)
+zeven = 44/4-4
+print("Zeven is", zeven)
+acht = (4*4)-(4+4)
+print("Acht is", acht)
+negen = (44/44)+4+4
+print("Negen is", negen)
+tien = (44-4)/4
+print("Tien is", tien)
+elf = 44/(sqrt(4)+sqrt(4))
+print("Elf is", elf)
+twaalf = 4+4+sqrt(4)+sqrt(4)
+print("Twaalf is", twaalf)
+dertien = 4+4+4 + (4/4)
+print("Dertien is", dertien)
+veertien = 4+4+4+sqrt(4)
+print("Veertien is", veertien)
+vijftien = (44/4)+4
+print("Vijftien is", vijftien)
+zestien = 4*4
+print("Zestien is", zestien)
+zeventien = (4*4)+(4/4)
+print("Zeventien is", zeventien)
+achttien = (4*4)+(4/sqrt(4))
+print("Achttien is", achttien)
+negentien = (factorial(4))-4-(4/4)
+print("Negentien is", negentien)
+twintig = 4*4+sqrt(4)+sqrt(4)
+print("Twintig is", twintig)
